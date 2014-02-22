c3b16fb8a9b1f9f0ba115462259b173e61629255
Stewart Dawson <stewart.dawson@gmail.com>
2014-02-22 18:58:40 -0500

    playing with topo generation

--- build_topo.sh
+++ build_topo.sh
@@ -11,8 +11,8 @@ ogr2ogr -f GeoJSON -clipdst json/city.json -where "name='River Clyde'" json/rive
 touch json/CommArea.json
 rm json/CommArea.json
 # ogr2ogr -f GeoJSON -clipsrc -4.1936 55.8379 -4.2274 55.8513 -where "highway<>''" json/CommArea.json glasgow/glasgow.osm-line.shp
-ogr2ogr -f GeoJSON -clipsrc -4.1936 55.8379 -4.2274 55.8513 json/CommArea.json scotland/landuse.shp
-#ogr2ogr -f GeoJSON -clipsrc -4.1936 55.8379 -4.2274 55.8513 -update -append json/CommArea.json scotland/roads.shp
+#ogr2ogr -f GeoJSON -clipsrc -4.1936 55.8379 -4.2274 55.8513 json/CommArea.json scotland/landuse.shp
+ogr2ogr -f GeoJSON -clipsrc -4.1936 55.8379 -4.2274 55.8513 json/CommArea.json scotland/roads.shp
 
 touch json/CommTransit.json
 rm json/CommTransit.json
